f64de1a474a2e2591f8abd59e9d46963a49e292b
NikhilSharma1234 <nikhil342015@gmail.com>
2025-03-08 19:20:41 -0800

    Modification to derivation and display name to account for both types

--- 2025_03_08_mapping.sql
+++ 2025_03_08_mapping.sql
@@ -3,4 +3,4 @@ VALUES
     ('Distance', 'Lidar Distance', 'Lidar_distance', 'dragino', 'lds12', 'centimeters', NULL, 'Lidar Distance from sensor', TRUE, 120),
     ('Distance', 'Lidar Signal', 'Lidar_signal', 'dragino', 'lds12', 'Custom Distance', NULL, 'Signal Strength from sensor', TRUE, 120),
     ('Distance', 'Lidar Temperature', 'Lidar_temp', 'dragino', 'lds12', 'celsius', NULL, 'Lidar Temperature from sensor', TRUE, 120),
-    ('Distance', 'Sonic Distance', 'Distance', 'dragino', 'dds45', 'millimeters', 'int(value) if isinstance(value, int) else int(value.split()[0])', 'Sonic Distance from sensor', TRUE, 120);
+    ('Distance', 'Sonic/Microwave Distance', 'Distance', 'dragino', 'all', 'millimeters', 'value if isinstance(value, int) else int(value.split()[0]) if isinstance(value, str) and value.endswith(" mm") else 0', 'Sonic Distance from sensor', TRUE, 120);
