ca2d5415c34eb605f5bea9affd404c7f9eb9dca5
NikhilSharma1234 <nikhil342015@gmail.com>
2025-03-08 18:55:38 -0800

    Insert script for 2 distance sensors

---
+++ 2025_03_08_mapping.sql
@@ -0,0 +1,6 @@
+INSERT INTO sensor_mapping (type, display_name, payload_value, brand, model, unit, derivation, description, sortable, width)
+VALUES
+    ('Distance', 'Lidar Distance', 'Lidar_distance', 'dragino', 'lds12', 'centimeters', NULL, 'Lidar Distance from sensor', TRUE, 120),
+    ('Signal Strength', 'Lidar Signal', 'Lidar_signal', 'dragino', 'lds12', 'Custom Distance', NULL, 'Signal Strength from sensor', TRUE, 120),
+    ('Temperature', 'Lidar Temperature', 'Lidar_temp', 'dragino', 'lds12', 'celsius', NULL, 'Lidar Temperature from sensor', TRUE, 120),
+    ('Distance', 'Sonic Distance', 'Distance', 'dragino', 'dds45', 'millimeters', 'int(value.split()[0])', 'Sonic Distance from sensor', TRUE, 120);
